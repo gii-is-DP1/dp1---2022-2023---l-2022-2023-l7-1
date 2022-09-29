e788e189975e8775479d57a92155b26c0028a7bc
AitorRD <aitroddue@alum.us.es>
2022-09-29 10:53:32 +0200

    add third owner

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -72,7 +72,7 @@ INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (12, 'Lucky', '2010
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (13, 'Sly', '2012-06-08', 1, 10);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (14, 'Perry', '2022-06-08', 2, 11);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (15, 'kamuy', '2021-07-08', 1, 12);
-INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (16, 'Steven', '2020-10-10', 4, 13);
+INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (16, 'Steven', '2020-10-10', 5, 13);
 
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (1, 7, '2013-01-01', 'rabies shot');
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (2, 8, '2013-01-02', 'rabies shot');

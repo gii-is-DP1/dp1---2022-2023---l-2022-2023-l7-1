1db1e56483275d5d15cf19f85d49d81f87427513
AitorRD <aitroddue@alum.us.es>
2022-09-29 10:48:49 +0200

    add third owner

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -14,6 +14,9 @@ INSERT INTO authorities(id,username,authority) VALUES (4,'fravilpae','owner');
 INSERT INTO users(username,password,enabled) VALUES ('jeszamgue','password',TRUE);
 INSERT INTO authorities(id,username,authority) VALUES (5,'jeszamgue','owner');
 
+INSERT INTO users(username,password,enabled) VALUES ('aitroddue','password',TRUE);
+INSERT INTO authorities(id,username,authority) VALUES (6,'aitroddue','owner');
+
 INSERT INTO vets(id, first_name,last_name) VALUES (1, 'James', 'Carter');
 INSERT INTO vets(id, first_name,last_name) VALUES (2, 'Helen', 'Leary');
 INSERT INTO vets(id, first_name,last_name) VALUES (3, 'Linda', 'Douglas');
@@ -51,6 +54,7 @@ INSERT INTO owners VALUES (9, 'David', 'Schroeder', '2749 Blackhawk Trail', 'Mad
 INSERT INTO owners VALUES (10, 'Carlos', 'Estaban', '2335 Independence La.', 'Waunakee', '6085555487', 'owner1');
 INSERT INTO owners VALUES (11, 'Francis', 'Villalobos', '112 Smith Ave.', 'Detroit', '622316650', 'fravilpae');
 INSERT INTO owners VALUES (12, 'Jesus', 'Zambrana', '9 Spain Ave.', 'Seville', '722870612', 'jeszamgue');
+INSERT INTO owners VALUES (13, 'Aitor', 'Rodriguez', '18 Diego St.', 'Seville', '654743409', 'aitroddue');
 
 
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (1, 'Leo', '2010-09-07', 1, 1);
@@ -68,7 +72,7 @@ INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (12, 'Lucky', '2010
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (13, 'Sly', '2012-06-08', 1, 10);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (14, 'Perry', '2022-06-08', 2, 11);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (15, 'kamuy', '2021-07-08', 1, 12);
-
+INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (16, 'Steven', '2020-10-10', 4, 13);
 
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (1, 7, '2013-01-01', 'rabies shot');
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (2, 8, '2013-01-02', 'rabies shot');

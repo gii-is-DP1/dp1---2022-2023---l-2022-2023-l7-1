f8fff57d0431127adaf0ac6a29138315e85210ee
Ramguerom <ramguerom@alum.us.es>
2022-10-27 16:21:38 +0200

    Actualizacion poderes

--- src/main/java/org/springframework/samples/petclinic/poderes/Poderes.java
+++ src/main/java/org/springframework/samples/petclinic/poderes/Poderes.java
@@ -6,8 +6,11 @@ import javax.persistence.Id;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Getter;
@@ -33,12 +36,16 @@ public class Poderes {
 
     @Column(name = "poder+-1")
     @NotEmpty
+    @Min(0)
+    @Max(6)
+    @Value("0")
     Integer poder1; //Si lo pongo con el +-1 da error ya que lo toma como operacion
 
     @Column(name = "poder?")
     @NotEmpty
+    @Value("0")
     Integer poder2; //Igual q el otro poder
-    //Aqui debemos poner q por defecto tiene el valor 0, pero ns como se pone
+  
 
 
     

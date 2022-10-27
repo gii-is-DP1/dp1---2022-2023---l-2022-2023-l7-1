d85668da56ea40361373da78df16b78e4448b573
fravilpae <fravilpae@alum.us.es>
2022-10-27 16:19:40 +0200

    Partida y usuario inicializados

--- src/main/java/org/springframework/samples/petclinic/usuario/UsuarioService.java
+++ src/main/java/org/springframework/samples/petclinic/usuario/UsuarioService.java
@@ -1,5 +1,13 @@
 package org.springframework.samples.petclinic.usuario;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class UsuarioService {
     
+    UsuarioRepository repo;
+
+    @Autowired
+    public UsuarioService(UsuarioRepository repo) {
+        this.repo = repo;
+    }
 }

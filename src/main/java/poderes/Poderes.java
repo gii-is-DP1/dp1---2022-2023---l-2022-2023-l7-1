39f695abdf09c0a9d83cccef03bc333382f4177f
Ramguerom <ramguerom@alum.us.es>
2022-10-27 09:05:14 +0200

    Creaccion poderes

---
+++ src/main/java/poderes/Poderes.java
@@ -0,0 +1,45 @@
+package poderes;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "poderes")
+public class Poderes {
+
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	protected Integer id;
+
+    @Column(name = "idPartida")
+    @NotEmpty
+    Integer idPartida;
+
+    @Column(name = "idJugador")
+    @NotEmpty
+    Integer idPJugador;
+
+    @Column(name = "poder+-1")
+    @NotEmpty
+    Integer poder1; //Si lo pongo con el +-1 da error ya que lo toma como operacion
+
+    @Column(name = "poder?")
+    @NotEmpty
+    Integer poder2; //Igual q el otro poder
+    //Aqui debemos poner q por defecto tiene el valor 0, pero ns como se pone
+
+
+    
+}
---
+++ src/main/java/poderes/PoderesController.java
@@ -0,0 +1,5 @@
+package poderes;
+
+public class PoderesController {
+    
+}

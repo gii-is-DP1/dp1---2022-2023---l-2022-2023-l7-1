5605864c73d8e3ba2612a0994be856d190ce76de
jRibasN <julribde@alum.us.es>
2022-11-07 18:51:09 +0100

    modificación tabla users

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -1,21 +1,21 @@
 -- One admin user, named admin1 with passwor 4dm1n and authority admin
-INSERT INTO usuarios(name, last_name, username, password, birth_date, email, phone, enabled) VALUES ('ad', 'min', 'admin1', '4dm1n', '25/02/1995', 'admin@gmail.com', '954734895', TRUE);
-INSERT INTO authorities(id,username,authority) VALUES (1,'admin1','admin');
+INSERT INTO usuarios(name, last_name, username, password, birth_date, email, phone, enabled) VALUES ('ad', 'min', 'admin1', '4dm1n', '2010-09-07', 'admin@gmail.com', '954734895', TRUE);
+--INSERT INTO authorities(id,username,authority) VALUES (1,'admin1','admin');
 -- One owner user, named owner1 with passwor 0wn3r
-INSERT INTO usuarios(name, last_name, username, password, birth_date, email, phone, enabled) VALUES ('ow', 'ner', 'owner1', '0wn3r', '27/01/1969', 'owner@gmail.com', '954734895', TRUE);
-INSERT INTO authorities(id,username,authority) VALUES (2,'owner1','owner');
+INSERT INTO usuarios(name, last_name, username, password, birth_date, email, phone, enabled) VALUES ('ow', 'ner', 'owner1', '0wn3r', '2010-09-07', 'owner@gmail.com', '954734895', TRUE);
+--INSERT INTO authorities(id,username,authority) VALUES (2,'owner1','owner');
 -- One vet user, named vet1 with passwor v3t
-INSERT INTO usuarios(name, last_name, username, password, birth_date, email, phone, enabled) VALUES ('vete', 'rinary', 'vet1', 'v3t', '04/11/1987', 'vet@gmail.com', '954734895', TRUE);
-INSERT INTO authorities(id,username,authority) VALUES (3,'vet1','veterinarian');
+INSERT INTO usuarios(name, last_name, username, password, birth_date, email, phone, enabled) VALUES ('vete', 'rinary', 'vet1', 'v3t', '2010-09-07', 'vet@gmail.com', '954734895', TRUE);
+--INSERT INTO authorities(id,username,authority) VALUES (3,'vet1','veterinarian');
 -- Owner user
-INSERT INTO usuarios(name, last_name, username, password, birth_date, email, phone, enabled) VALUES ('Francis', 'Villalobos', 'fravilpae', 'password', '14/06/2002', 'fravilpae@alum.us.es', '954734895', TRUE);
-INSERT INTO authorities(id,username,authority) VALUES (4,'fravilpae','owner');
+INSERT INTO usuarios(name, last_name, username, password, birth_date, email, phone, enabled) VALUES ('Francis', 'Villalobos', 'fravilpae', 'password', '2010-09-07', 'fravilpae@alum.us.es', '954734895', TRUE);
+--INSERT INTO authorities(id,username,authority) VALUES (4,'fravilpae','owner');
 
-INSERT INTO usuarios(name, last_name, username, password, birth_date, email, phone, enabled) VALUES ('Jesus', 'Zambrana', 'jeszamgue', 'password', '07/02/2002', 'jeszamgue@alum.us.es', '954734895', TRUE);
-INSERT INTO authorities(id,username,authority) VALUES (5,'jeszamgue','owner');
+INSERT INTO usuarios(name, last_name, username, password, birth_date, email, phone, enabled) VALUES ('Jesus', 'Zambrana', 'jeszamgue', 'password', '2010-09-07', 'jeszamgue@alum.us.es', '954734895', TRUE);
+--INSERT INTO authorities(id,username,authority) VALUES (5,'jeszamgue','owner');
 
-INSERT INTO usuarios(name, last_name, username, password, birth_date, email, phone, enabled) VALUES ('Aitor', 'Rodriguez', 'aitroddue', 'password', '15/05/2002', 'aitroddue@alum.us.es', '954734895', TRUE);
-INSERT INTO authorities(id,username,authority) VALUES (6,'aitroddue','owner');
+INSERT INTO usuarios(name, last_name, username, password, birth_date, email, phone, enabled) VALUES ('Aitor', 'Rodriguez', 'aitroddue', 'password', '2010-09-07', 'aitroddue@alum.us.es', '954734895', TRUE);
+--INSERT INTO authorities(id,username,authority) VALUES (6,'aitroddue','owner');
 
 INSERT INTO vets(id, first_name,last_name) VALUES (1, 'James', 'Carter');
 INSERT INTO vets(id, first_name,last_name) VALUES (2, 'Helen', 'Leary');

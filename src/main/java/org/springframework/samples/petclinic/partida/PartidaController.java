77ea1f5268dc930ea22627d92d133899b8cbac09
AitorRD <aitroddue@alum.us.es>
2022-12-01 13:17:24 +0100

    lobby (invitacion partida) - version 1

--- src/main/java/org/springframework/samples/petclinic/partida/PartidaController.java
+++ src/main/java/org/springframework/samples/petclinic/partida/PartidaController.java
@@ -3,14 +3,17 @@ package org.springframework.samples.petclinic.partida;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.user.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PartidaController {
 
     private static final String VIEW_CREAR_PARTIDA = "partidas/crearPartida";
+    private static final String VIEW_AMIGOS = "partidas/lobby";
 
     
     private PartidaService service;
@@ -21,9 +24,19 @@ public class PartidaController {
     }
 
     @Transactional
-	@GetMapping(value = "/crearPartida")
-	public ModelAndView creacionPartida(){
-		ModelAndView res = new ModelAndView(VIEW_CREAR_PARTIDA);
-		return res;
+	@GetMapping(value = "/{username}/crearPartida")
+	public ModelAndView creacionPartida(@PathVariable("username") String username){
+		ModelAndView mav = new ModelAndView(VIEW_CREAR_PARTIDA);
+		mav.addObject("username", username);
+		return mav;
 	}
+
+    @Transactional
+	@GetMapping(value = "/{username}/lobby")
+	public ModelAndView mostrarAmigos(@PathVariable("username") String username){
+		ModelAndView mav = new ModelAndView(VIEW_AMIGOS);
+		mav.addObject("username", username);
+		return mav;
+	}
+    
 }

45b6f17e74d6bdb796f3ea4779f4158999fceb31
jeszamgue <jeszamgue@alum.us.es>
2023-01-10 23:34:47 +0100

    correccion PartidaControllerTest

--- src/main/java/org/springframework/samples/petclinic/Chat/Mensaje.java
+++ src/main/java/org/springframework/samples/petclinic/Chat/Mensaje.java
@@ -1,4 +1,4 @@
-package org.springframework.samples.petclinic.Chat;
+package org.springframework.samples.petclinic.chat;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
--- src/main/java/org/springframework/samples/petclinic/partida/PartidaService.java
+++ src/main/java/org/springframework/samples/petclinic/partida/PartidaService.java
@@ -13,10 +13,10 @@ import java.util.stream.IntStream;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.samples.petclinic.Chat.Chat;
-import org.springframework.samples.petclinic.Chat.ChatService;
 import org.springframework.samples.petclinic.accion.Accion;
 import org.springframework.samples.petclinic.accion.AccionService;
+import org.springframework.samples.petclinic.chat.Chat;
+import org.springframework.samples.petclinic.chat.ChatService;
 import org.springframework.samples.petclinic.criterios.CriterioA1;
 import org.springframework.samples.petclinic.criterios.CriterioA2;
 import org.springframework.samples.petclinic.criterios.CriterioA3;

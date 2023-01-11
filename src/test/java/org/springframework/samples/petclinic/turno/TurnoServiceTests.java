236d95e04c534a0348adc5c14d5f6e342d9fee23
Ramguerom <ramguerom@alum.us.es>
2023-01-11 12:47:01 +0100

    CorreccionLanzamientoConjuntoTestTurnoService

--- src/test/java/org/springframework/samples/petclinic/turno/TurnoServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/turno/TurnoServiceTests.java
@@ -4,8 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +15,7 @@ import org.springframework.samples.petclinic.turnos.Turno;
 import org.springframework.samples.petclinic.turnos.TurnoService;
 import org.springframework.samples.petclinic.util.Territorio;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
 class TurnoServiceTests {
@@ -33,61 +32,29 @@ class TurnoServiceTests {
 
     @BeforeEach
     public void setPartida(){
-        partida.setDateTime(LocalDateTime.of(2023, 1, 4, 17, 55));
-        partida.setId(2);
-        partida.setIdCriterioA1(2);
-        partida.setIdCriterioA2(1);
-        partida.setIdCriterioB1(5);
-        partida.setIdCriterioB2(3);
-        partidaService.savePartida(partida);
         turno.setId(7);
         turno.setNumTerritoriosJ1(0);
         turno.setNumTerritoriosJ2(0);
         turno.setNumTerritoriosJ3(0);
         turno.setNumTerritoriosJ4(0);
-        turno.setPartida(partida);
+        turno.setPartida(partidaService.getPartidaById(1));
         turno.setTerritorio(Territorio.BOSQUE);
+        turnoService.saveTurno(turno);
     }
 
-    @Test
-    @Transactional
-    public void shouldSaveTurno(){
-        Boolean turnoNotSave = turnoService.getTurnosByPartida(2).isEmpty();
-        assertThat(turnoNotSave).isTrue();
-        turnoService.saveTurno(turno);
-        Boolean turnoSave = turnoService.getTurnosByPartida(2).isEmpty();
-        assertThat(turnoSave).isFalse();
-    }
 
     @Test
     @Transactional
     public void shouldGetTurnoById(){
-        Turno turno1 = turnoService.getTurnoById(7);
-        assertThat(turno1).isNull();
-        turnoService.saveTurno(turno);
-        Turno turno2 = turnoService.getTurnoById(7);
+        Turno turno2 = turnoService.getTurnoById(1);
         assertThat(turno2).isNotNull();
     }
 
     @Test
     @Transactional
     public void shouldGetTurnosByPartida(){
-        List<Turno> turno1 = turnoService.getTurnosByPartida(2);
-        assertThat(turno1).isEmpty();
-        turnoService.saveTurno(turno);
-        List<Turno> turno2 = turnoService.getTurnosByPartida(2);
+        List<Turno> turno2 = turnoService.getTurnosByPartida(1);
         assertThat(turno2).isNotEmpty();
-    }
-
-    @Test
-    @Transactional
-    public void shouldDelete(){
-        turnoService.saveTurno(turno);
-        Turno turno1 = turnoService.getTurnoById(7);
-        assertThat(turno1).isNotNull();
-        turnoService.delete(turno);
-        Turno turno2 = turnoService.getTurnoById(7);
-        assertThat(turno2).isNull();
     }
 
 }

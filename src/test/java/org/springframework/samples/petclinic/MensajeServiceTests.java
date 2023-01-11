abf65202e2d433b2501cd22941afb5493d7238de
diegarlin <garcialinaresdiego@gmail.com>
2023-01-11 21:35:28 +0100

    ChatServiceTest hecho

--- src/test/java/org/springframework/samples/petclinic/MensajeServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/MensajeServiceTests.java
@@ -7,10 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.NoSuchElementException;
 
-import org.junit.Before;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +15,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.samples.petclinic.chat.Mensaje;
 import org.springframework.samples.petclinic.chat.MensajeService;
-import org.springframework.samples.petclinic.partida.Partida;
-import org.springframework.samples.petclinic.partida.PartidaService;
 import org.springframework.samples.petclinic.user.User;
 import org.springframework.samples.petclinic.user.UserService;
 import org.springframework.stereotype.Service;
@@ -32,7 +27,6 @@ public class MensajeServiceTests {
 
     @Autowired
     protected UserService userService;
-
 
     protected User user = new User();
 
